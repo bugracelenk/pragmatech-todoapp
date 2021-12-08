91ab9571fe1c00beebc4af182ae84e2e4aecaaee
Altuğ Buğra Çelenk <33699679+bugracelenk@users.noreply.github.com>
2021-12-08 15:42:02 +0300

    Update main.ts

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Pragmatech Todo App v1')
-    .setDescription('The Pragmatech Todo API description')
+    .setTitle('Todo App v1')
+    .setDescription('The Todo API description')
     .setVersion('1.0')
     .addTag('todo')
     .build();
